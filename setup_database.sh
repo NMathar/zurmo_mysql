3fb9b748bf34d83bd5e3a9e0f9fb3627dc670232
NMonst4 <Nico.Mathar@gmail.com>
2018-06-15 16:40:09 +0200

    rm tables and clear script

--- setup_database.sh
+++ setup_database.sh
@@ -1,11 +1,5 @@
 #!/bin/bash
 
-#service mysql start
-#mysql < /create_db.sql
-#mysql < /create_user.sql
-#mysql zurmo < /zurmo.sql
-#service mysql stop
-
 chown -R mysql:mysql /var/lib/mysql
 
 mysqld_safe
